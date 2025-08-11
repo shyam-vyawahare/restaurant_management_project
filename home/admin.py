f53bcc9632d97b7c4468e51ea6d8be238f34756f
Shyam Vyawahare <Shyamvyawahare1@gmail.com>
2025-08-11 17:58:44 +0530

    25812

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Restaurant
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'opening_time', 'closing_time')
+    fields = ('name', 'description', 'logo', 'opening_time', 'closing_time')
