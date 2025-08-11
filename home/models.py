19a367596da293ef5bf6a392fd33589e8aa878fc
Shyam Vyawahare <Shyamvyawahare1@gmail.com>
2025-08-11 17:27:36 +0530

    Update models.py

--- home/models.py
+++ home/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 # views.py
 from django.shortcuts import render
-from .models import Restaurant  # Assuming you have a Restaurant model
+from .models import Restaurant 
 
 def home_view(request):
     try:
