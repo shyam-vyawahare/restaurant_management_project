6e079d220f856f94f0c51aaff5d9f30fe53f7468
Shyam Vyawahare <Shyamvyawahare1@gmail.com>
2025-08-11 17:45:04 +0530

    25812

--- home/models.py
+++ home/models.py
@@ -1,18 +1,11 @@
 from django.db import models
 
 # Create your models here.
-# views.py
-from django.shortcuts import render
-from .models import Restaurant
 
-def home_view(request):
-    try:
-        # Get the first restaurant (or filter as needed)
-        restaurant = Restaurant.objects.first()
-        context = {
-            'restaurant_name': restaurant.name if restaurant else "Default Restaurant"
-        }
-    except Exception as e:
-        context = {'restaurant_name': "Default Restaurant"}
-    
-    return render(request, 'home.html', context)
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100, default="Tasty Bites")
+    description = models.TextField(blank=True)
+    logo = models.ImageField(upload_to='restaurant/', blank=True)
+                
+    def __str__(self):
+        return self.name
