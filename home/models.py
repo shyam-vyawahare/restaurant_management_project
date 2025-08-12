3c793d1164455a9a9d4c70176f1c5bc61860eb02
Shyam Vyawahare <Shyamvyawahare1@gmail.com>
2025-08-12 18:04:39 +0530

    Update models.py

--- home/models.py
+++ home/models.py
@@ -10,5 +10,9 @@ class Restaurant(models.Model):
     opening_time = models.TimeField(blank=True, null=True)
     closing_time = models.TimeField(blank=True, null=True)
 
+# About us 
+    about_us = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='about/', blank=True, null=True)
+
     def __str__(self):
         return self.name
