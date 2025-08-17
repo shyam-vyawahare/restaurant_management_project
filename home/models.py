9bd26d4f1349e299d9bcae97f597da0340413090
Shyam Vyawahare <Shyamvyawahare1@gmail.com>
2025-08-17 22:23:59 +0530

    33676

--- home/models.py
+++ home/models.py
@@ -1,19 +1,10 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 class Restaurant(models.Model):
-    name = models.CharField(max_length=100, default="Tasty Bites")
+    name = models.CharField(max_length=100, default="Our Restaurant")
     description = models.TextField(default="Welcome to our restaurant!")
-	   phone = models.CharField(max_length=20, blank=True, null=True)  # Add this line
+    phone = models.CharField(max_length=20, blank=True, null=True)  # Add this line
     logo = models.ImageField(upload_to='restaurant/', blank=True, null=True)
-    opening_time = models.TimeField(blank=True, null=True)
-    closing_time = models.TimeField(blank=True, null=True)
-
-# About us 
-    about_us = models.TextField(blank=True, null=True)
-    image = models.ImageField(upload_to='about/', blank=True, null=True)
-
+                    
     def __str__(self):
         return self.name
