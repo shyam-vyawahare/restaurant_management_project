bbe4854a7810058da7b0b89a67da12ab1a0c0b0b
Shyam Vyawahare <Shyamvyawahare1@gmail.com>
2025-08-17 22:10:09 +0530

    Update models.py

--- home/models.py
+++ home/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=100, default="Tasty Bites")
-    description = models.TextField(blank=True, null=True)
+    description = models.TextField(default="Welcome to our restaurant!")
     logo = models.ImageField(upload_to='restaurant/', blank=True, null=True)
     opening_time = models.TimeField(blank=True, null=True)
     closing_time = models.TimeField(blank=True, null=True)
