cbf078666c30ed5b5c85f58246568bf3f5c0cbdd
Shyam Vyawahare <Shyamvyawahare1@gmail.com>
2025-08-17 22:52:58 +0530

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -7,8 +7,8 @@ from django.urls import path, include
 urlpatterns = [
     path('', home_view, name='home'),
     path('about/', about_view, name='about'),
+			path('menu/', menu_view, name='menu'),
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
 path('contact/', contact_view, name='contact'),
 ]
-
