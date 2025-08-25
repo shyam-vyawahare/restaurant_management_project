e99ae84a5c481b68ca8c2829ab458ee5829edeab
Shyam Vyawahare <Shyamvyawahare1@gmail.com>
2025-08-25 17:58:47 +0530

    37180

--- home/context_processor.py
+++ home/context_processor.py
@@ -11,6 +11,12 @@ def global_context(request):
         restaurant = Restaurant.objects.first()
     except:
         restaurant = None
+
+    # Define opening hours (can be moved to settings or database later)
+    opening_hours = {
+        'weekdays': 'Mon - Fri: 11:00 AM - 9:00 PM',
+        'weekend': 'Sat - Sun: 10:00 AM - 10:00 PM'
+    }                                       
     
     return {
         'current_year': datetime.now().year,
