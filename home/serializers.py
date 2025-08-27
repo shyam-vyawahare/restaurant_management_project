d9a6467d9fbb0775419c2c63cd2f15667d2392c3
Shyam Vyawahare <Shyamvyawahare1@gmail.com>
2025-08-27 14:05:42 +0530

    Create serializers.py
    Serializers added

---
+++ home/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+class MenuItemSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField(max_length=100)
+    description = serializers.CharField()
+    price = serializers.DecimalField(max_digits=6, decimal_places=2)
+    category = serializers.CharField(max_length=50)
+    is_vegetarian = serializers.BooleanField()
+    is_available = serializers.BooleanField()
