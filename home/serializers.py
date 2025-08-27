d11c7231b35f084edda4759b1f9b0ee1090c44df
Shyam Vyawahare <Shyamvyawahare1@gmail.com>
2025-08-27 14:14:23 +0530

    39159

--- home/serializers.py
+++ home/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+# Serializer code
 class MenuItemSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     name = serializers.CharField(max_length=100)
