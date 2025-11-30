cc174f9401ecff38c5b6584b95e6d26978e6ef2d
Shyam-Vyawahare <shyamvyawahare1@gmail.com>
2025-11-30 17:27:11 +0530

    chore: update settings and urls after restructuring

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -1,36 +1,27 @@
 """
 URL configuration for restaurant_management project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import handler404
 from home.views import custom_404
 
-handler404 = custom_404  # Add this line at the bottom
+# Custom error handler
+handler404 = custom_404
 
 urlpatterns = [
+    # Admin
     path('admin/', admin.site.urls),
-    path('api/',include('home.urls')),
-    path('api/accounts/',include('account.urls')),
-    path('api/products/',include('products.urls')),
-    path('api/orders/',include('orders.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+    # API routes
+    path('api/', include('home.urls')),                         # Home / general routes
+    path('api/accounts/', include('account.urls')),             # User authentication / profiles
+    path('api/products/', include('home.products.urls')),       # Updated path after moving products under home/
+    path('api/orders/', include('orders.urls')),                # Orders app
+]
 
-
-
+# Media file handling (DEV mode only)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
